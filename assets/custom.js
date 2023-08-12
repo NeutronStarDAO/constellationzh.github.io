955833153a2967dd374e7ac92af1e63ca0f61cab
Nash Zheng <nashzhengpro@gmail.com>
2023-08-12 22:44:50 +0800

    update

--- assets/custom.js
+++ assets/custom.js
@@ -118,19 +118,19 @@ var initAll = function () {
     const btn1 = document.querySelector('.box1');
     if (btn1) {
         btn1.addEventListener('click', () => {
-            window.location.href = '/0.去中心化之旅/区块链是啥？.html';
+            window.location.href = '/constellationzh.github.io/0.去中心化之旅/区块链是啥？.html';
         });
     }
-    const btn2 = document.querySelector('.box2'); 
+    const btn2 = document.querySelector('.box2');
     if (btn2) {
         btn2.addEventListener('click', () => {
-            window.location.href = '/1.了解IC/1.了解IC.html';
+            window.location.href = '/constellationzh.github.io/1.了解IC/1.了解IC.html';
         });
     }
     const btn3 = document.querySelector('.box3');
     if (btn3) {
         btn3.addEventListener('click', () => {
-            window.location.href = '/0.去中心化之旅/造梦家的冒险之旅.html';
+            window.location.href = '/constellationzh.github.io/0.去中心化之旅/造梦家的冒险之旅.html';
         });
     }
 
